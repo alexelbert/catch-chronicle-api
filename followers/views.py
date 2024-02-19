5bf6ca248f62826101185e982444d9169bf596ae
Alexander Elbert <alexander91elbert@gmail.com>
2024-02-19 15:56:42 +0100

    Create views for followers. Logged in user can follow/unfollow.

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from catch_chronicle.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
 
-# Create your views here.
+
+class FollowerList(generics.ListCreateAPIView):
+    """
+    List follower or create new if user is logged in.
+    """
+    
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieving a follower, delete (unfollow) if user is owner.
+    """
+    
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
+    permission_classes = [IsOwnerOrReadOnly]
