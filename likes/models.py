f9e77617032d83b046555dd067ba5966f010aa0c
Alexander Elbert <alexander91elbert@gmail.com>
2024-02-19 15:17:58 +0100

    Create model for likes, related to owner and catch.

--- likes/models.py
+++ likes/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from catches.models import Catch
 
-# Create your models here.
+
+class Like(models.Model):
+    """
+    Like model, related to owner and Catch.
+    """
+    
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    catch = models.ForeignKey(Catch, related_name='likes', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'catch']
+    
+    def __str__(self):
+        return f'{owner} {self.catch}'
