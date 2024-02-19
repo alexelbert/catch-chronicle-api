4870793e1b760d3826f3899073bf1193fce586b9
Alexander Elbert <alexander91elbert@gmail.com>
2024-02-19 13:24:23 +0100

    Add fields to the serializer, is_owner for user requests.

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -3,10 +3,30 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    bio = serializers.ReadOnlyField()
+    name = serializers.ReadOnlyField()
+    catches_count = serializers.ReadOnlyField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+    
 
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'name',
-            'bio', 'location', 'profile_picture', 'facebook_url', 'twitter_url', 'instagram_url',
+            'id', 
+            'owner',
+            'is_owner', 
+            'created_at', 
+            'updated_at', 
+            'catches_count',
+            'name',
+            'bio', 
+            'location', 
+            'profile_picture', 
+            'facebook_url', 
+            'twitter_url', 
+            'instagram_url',
         ]
