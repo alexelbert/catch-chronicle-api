23b62e17bf998dd6d5afc88adf3f42455944eb2c
Alexander Elbert <alexander91elbert@gmail.com>
2024-02-19 14:54:35 +0100

    Create model for comments in relation to User and Catch.

---
+++ comments/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+from catches.models import Catch
+
+
+class Comment(models.Model):
+    """
+    Comment model, related to User and Catch.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    catch = models.ForeignKey(Catch, on_delete=models.CASCADE)
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.content
