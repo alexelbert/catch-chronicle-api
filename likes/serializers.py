2003e6ce339c24dada9987e8fe9b3c2c4b939aee
Alexander Elbert <alexander91elbert@gmail.com>
2024-02-19 15:20:48 +0100

    Create serializers for Like model, validation checking against liking a catch several times.

---
+++ likes/serializers.py
@@ -0,0 +1,29 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from likes.models import Like
+
+class LikeSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Like model.
+    Included method for making sure user can only make one like per catch.
+    """
+    
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+
+    class Meta:
+        model = Like
+        fields = [
+            'id', 
+            'created_at', 
+            'owner', 
+            'catch'
+        ]
+    
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'You have already liked this catch.'
+            })
