d18af2b5beb1f5f570d011c875f7489a033280f4
Alexander Elbert <alexander91elbert@gmail.com>
2024-02-19 15:50:02 +0100

    Create follower model, relationship between users who is following whom.

--- followers/models.py
+++ followers/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Follower(models.Model):
+    """
+    Model for the relationship between users, indicating who is following whom.
+    """
+    
+    owner = models.ForeignKey(
+        User, related_name='following', on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    followed = models.ForeignKey(
+        User, related_name='followed', on_delete=models.CASCADE
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'followed']
+
+    def __str__(self):
+        return f'{self.owner} {self.followed}'
+    
