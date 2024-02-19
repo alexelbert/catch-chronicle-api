cd5657debf68a244625660af62f6a51bd87b8e89
Alexander Elbert <alexander91elbert@gmail.com>
2024-02-19 16:24:14 +0100

    Add get following id to profile serializer  for retrieving follower counts to the profile.

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
@@ -7,10 +8,22 @@ class ProfileSerializer(serializers.ModelSerializer):
     bio = serializers.ReadOnlyField()
     name = serializers.ReadOnlyField()
     catches_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+    
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            print(following)
+            return following.id if following else None
+        return None
     
 
     class Meta:
@@ -29,4 +42,6 @@ class ProfileSerializer(serializers.ModelSerializer):
             'facebook_url', 
             'twitter_url', 
             'instagram_url',
+            'following_count',
+            'followers_count',
         ]
