723d3950e3fc5bd5ad040e2d39cca24f42f5717a
Alexander Elbert <alexander91elbert@gmail.com>
2024-02-23 16:43:56 +0100

    Add name parameters to paths for better referencing, also authentication.

--- catches/urls.py
+++ catches/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from catches import views
 
 urlpatterns = [
-    path('catches/', views.CatchList.as_view()),
-    path('catches/<int:pk>/', views.CatchDetail.as_view()),
+    path('catches/', views.CatchList.as_view(), name='catch'),
+    path('catches/<int:pk>/', views.CatchDetail.as_view(), name='catch_detail'),
 ]
