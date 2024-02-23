317c382aab7c28ebd8f22b6202647f27f62cd95e
Alexander Elbert <alexander91elbert@gmail.com>
2024-02-23 16:37:09 +0100

    Add latitude/ longitude for geolocation access, remove image filters and privacy choices.

--- catches/models.py
+++ catches/models.py
@@ -4,11 +4,8 @@ from django.contrib.auth.models import User
 class Catch(models.Model):
     """
     Model to represent a catch made by a user.
+    Containing geolocation data and predefined lists. 
     """
-    PRIVACY_CHOICES = [
-        ('public', 'Public'),
-        ('private', 'Private'),
-    ]
     
     METHOD_CHOICES = [
         ('flyrod', 'Fly Rod'),
@@ -23,43 +20,25 @@ class Catch(models.Model):
         ('stormy', 'Stormy'),
     ]
 
-    FILTER_CHOICES = [
-        ('_1977', '1977'), ('brannan', 'Brannan'),
-        ('earlybird', 'Earlybird'), ('hudson', 'Hudson'),
-        ('inkwell', 'Inkwell'), ('lofi', 'Lo-Fi'),
-        ('kelvin', 'Kelvin'), ('normal', 'Normal'),
-        ('nashville', 'Nashville'), ('rise', 'Rise'),
-        ('toaster', 'Toaster'), ('valencia', 'Valencia'),
-        ('walden', 'Walden'), ('xpro2', 'X-pro II')
-    ]
-
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    image = models.ImageField(upload_to='images/', default='../default_profile_zsweuz', blank=True)
-    image_filter = models.CharField(max_length=32, choices=FILTER_CHOICES, default='normal')
+    caption = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/', default='../default_profile_zsweuz', blank=False)
 
     species = models.CharField(max_length=100)
     method = models.CharField(max_length=20, choices=METHOD_CHOICES)
     weight = models.DecimalField(max_digits=6, decimal_places=2)
     length = models.DecimalField(max_digits=6, decimal_places=2)
     location = models.CharField(max_length=255)
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
     time = models.TimeField()
     weather = models.CharField(max_length=20, choices=WEATHER_CHOICES)
     lure = models.CharField(max_length=50)
-
-    method_privacy = models.CharField(max_length=10, choices=PRIVACY_CHOICES, default='private')
-    weight_privacy = models.CharField(max_length=10, choices=PRIVACY_CHOICES, default='private')
-    length_privacy = models.CharField(max_length=10, choices=PRIVACY_CHOICES, default='private')
-    location_privacy = models.CharField(max_length=10, choices=PRIVACY_CHOICES, default='private')
-    time_privacy = models.CharField(max_length=10, choices=PRIVACY_CHOICES, default='private')
-    weather_privacy = models.CharField(max_length=10, choices=PRIVACY_CHOICES, default='private')
-    lure_privacy = models.CharField(max_length=10, choices=PRIVACY_CHOICES, default='private')
 
     class Meta:
         ordering = ['-created_at']
 
     def __str__(self):
-        return f'{self.owner.username}\'s catch: {self.title}'
+        return f'{self.owner.username}\'s catch: {self.caption}'
