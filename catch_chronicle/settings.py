35f6ffbab2274153605c3de868552ac3313311b8
Alexander Elbert <alexander91elbert@gmail.com>
2024-03-04 17:09:18 +0100

    Reset migrations.

--- catch_chronicle/settings.py
+++ catch_chronicle/settings.py
@@ -157,7 +157,6 @@ else:
         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
