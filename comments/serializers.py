3fd98cb4800d165801fbfe6ff9d44ad37a11a0f0
Alexander Elbert <alexander91elbert@gmail.com>
2024-03-04 10:05:04 +0100

    Revert name change.

--- comments/serializers.py
+++ comments/serializers.py
@@ -31,7 +31,7 @@ class CommentSerializer(serializers.ModelSerializer):
             'owner',
             'is_owner',
             'profile_id',
-            'profile_picture',
+            'profile_image',
             'created_at',
             'updated_at',
             'catch',
