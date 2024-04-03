84137c0205f34a68cf43ef7b1575c68824c2fd4d
Alexander Elbert <alexander91elbert@gmail.com>
2024-04-03 13:40:35 +0200

    Add IsNotificationOwner permission to limit notification access to the notification owner.

--- catch_chronicle/permissions.py
+++ catch_chronicle/permissions.py
@@ -3,7 +3,23 @@ from rest_framework.exceptions import PermissionDenied
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to limit object editing exclusively to its owner.
+    """
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.owner == request.user
+
+class IsNotificationOwner(permissions.BasePermission):
+    """
+    Granting custom permissions to limit 
+    notification access only to the notification owner.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        if obj.user == request.user:
+            return True
+        raise PermissionDenied(
+            "403 Permission denied"
+        )
