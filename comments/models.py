4e62bb238fd95a1752462d526f566b2db6e8a10c
Alexander Elbert <alexander91elbert@gmail.com>
2024-04-03 13:38:26 +0200

    Add notification creation on Comment model save

--- comments/models.py
+++ comments/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from catches.models import Catch
+from notifications.models import Notification
 
 
 class Comment(models.Model):
@@ -18,3 +19,13 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.content
+
+    def save(self, *args, **kwargs):
+        # Creates a notification when a comment is added
+        if not self.pk:  # Only on creation
+            Notification.objects.create(
+                user=self.catchId.owner,
+                notification_type='Comment',
+                notification_text=f'{self.owner.username} commented on your catch.'
+            )
+        super().save(*args, **kwargs)
