7bd1d47596b8d816dc0b4a76f2f9290555b24196
Alexander Elbert <alexander91elbert@gmail.com>
2024-04-07 12:49:29 +0200

    Add natural time for the notifications.

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Notification
 
@@ -6,8 +7,12 @@ class NotificationSerializer(serializers.ModelSerializer):
     Serializer for Notification model.
     """
     user = serializers.ReadOnlyField(source="user.username")
+    created_at = serializers.SerializerMethodField()  # Modify the created_at field
 
     class Meta:
         model = Notification
         fields = ('id', 'user', 'created_at', 'notification_type', 'notification_text', 'is_read')
         read_only_fields = ('id', 'user', 'created_at', 'notification_type', 'notification_text', 'is_read')
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
