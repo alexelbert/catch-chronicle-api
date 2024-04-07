8fd650c83da8ca7fe5fb8bdf215f1c041dd838b1
Alexander Elbert <alexander91elbert@gmail.com>
2024-04-07 19:00:31 +0200

    Change code after linter, code make simpler, updated docstring.

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -2,17 +2,23 @@ from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Notification
 
+
 class NotificationSerializer(serializers.ModelSerializer):
     """
-    Serializer for Notification model.
+    Serializes Notification instances, displaying usernames and using
+    naturaltime for readable timestamps. Fields are read-only to
+    prevent modification via API.
     """
     user = serializers.ReadOnlyField(source="user.username")
-    created_at = serializers.SerializerMethodField()  # Modify the created_at field
+    created_at = serializers.SerializerMethodField()
 
     class Meta:
         model = Notification
-        fields = ('id', 'user', 'created_at', 'notification_type', 'notification_text', 'is_read')
-        read_only_fields = ('id', 'user', 'created_at', 'notification_type', 'notification_text', 'is_read')
+        fields = (
+            'id', 'user', 'created_at', 'notification_type',
+            'notification_text', 'is_read'
+        )
+        read_only_fields = fields
 
     def get_created_at(self, obj):
         return naturaltime(obj.created_at)
