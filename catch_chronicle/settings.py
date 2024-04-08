5c7ecb91ad3318b8b8ff12dcf394bbe1c39e1caf
Alexander Elbert <alexander91elbert@gmail.com>
2024-04-08 21:03:52 +0200

    Reduced page size from 20 to 15.

--- catch_chronicle/settings.py
+++ catch_chronicle/settings.py
@@ -36,7 +36,7 @@ REST_FRAMEWORK = {
     )],
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
+    'PAGE_SIZE': 15,
     'DATETIME_FORMAT': '%d %b %Y',
 }
 if 'DEV' not in os.environ:
